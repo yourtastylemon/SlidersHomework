5441dda49b88cedfd8144a8d2e61c87ac85d9e1b
Daria Kurokhtina <dashalemon96@gmail.com>
2021-08-22 21:42:47 +0300

    Initial Commit

---
+++ SlidersHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlidersHomework
+//
+//  Created by Дарья Курохтина on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
