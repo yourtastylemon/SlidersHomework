d0e37c1ee2629653010881a82eead7782afd985a
Daria Kurokhtina <dashalemon96@gmail.com>
2021-08-22 22:27:05 +0300

    setup outlets for labels, sliders and view

--- SlidersHomework/ViewController.swift
+++ SlidersHomework/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var colourView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colourView.layer.cornerRadius = 15
     }
 
 
